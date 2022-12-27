7eb7583675f1246956b56d29943e09104f6c40bf
Dhruba-jyoti-dutta <sddhruba.857@gmail.com>
2022-12-27 13:28:21 +0530

    Final(third) commit

--- wikipedia.py
+++ wikipedia.py
@@ -1,11 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
 import string
-def wikibot(url,inp):
+
+def wikibot(url,inp2):
     html_text=requests.get(url).text
     soup=BeautifulSoup(html_text,'html.parser')
     short_description=soup.find('div',class_='shortdescription nomobile noexcerpt noprint searchaux').text
-    with open(inp,'w', encoding="utf-8") as f: 
+    with open(inp2,'w', encoding="utf-8") as f: 
         f.write(f"{name} is a {short_description}\n")
         f.write("\n")
         details=soup.find_all('table',class_='infobox') 
@@ -18,16 +19,22 @@ def wikibot(url,inp):
                     f.write(f"{x.text} :: {y.text} \n")
         for k in range(1,4):
             f.writelines(f"{soup.find_all('p')[k].text}")
+
+
 if __name__=="__main__":
     while True:
-        i=input("search the wikipedia : ")
-        name=string.capwords(i)
-        lst=name.split()
-        fnl='_'.join(lst)
-        url="https://en.wikipedia.org/wiki/"+name
-        inp=input('''enter the destination path where you want to store your
-    data and file name in this format(F:/python/projects/ML/search results.txt):\n''')
-        wikibot(url,inp)
-
-        #try wikipedia 
-        # except
+        try:
+            inp1=input("search the wikipedia : ")
+            name=string.capwords(inp1)
+            lst_name=name.split()
+            fnl_name='_'.join(lst_name)
+            url="https://en.wikipedia.org/wiki/"+fnl_name
+            inp2=input('''enter the destination path where you want to store your data and file name in this format(F:/python/projects/ML/search results.txt):\n''')
+            wikibot(url,inp2)
+        except Exception as e:
+            print(f'''The thing you are searching for is not on wikipedia.Please try searching another word Thank you''')
+        finally:
+            press_button=input("Press enter to search more (q to quit):\n")
+            if press_button=='q':
+                exit()
+    
